12d50fdb588de31427eb0d2f148c0f5e0b65a5f1
adityanatu1 <79590144+adityanatu1@users.noreply.github.com>
2023-07-19 15:21:21 -0400

    Update Exo_NewNorm.R
    Cleaned Up comments

--- BV2_Juliet_EV_Proteome_Analysis/New_Norm_Data_Exosome_Regressed_Outputs/Code/Exo_NewNorm.R
+++ BV2_Juliet_EV_Proteome_Analysis/New_Norm_Data_Exosome_Regressed_Outputs/Code/Exo_NewNorm.R
@@ -1,16 +1,13 @@
 #####cell lysates from my BV2 polarization experiment Data Analysis - Aditya Natu - Rangaraju Lab
+ 
 
-###Data: mass spec data on the cell lysates from my BV2 polarization experiment (with gene names on the far right column). From this I would like to see if the treatments I added (LPS, TGFB, IL-10) did in fact polarize my BV2 cells.
-
-###Required: 
-
+##################Setting the work space##################
 cat("\014")
 rm(list = ls())
 dev.off()
 
-##################Setting the work space##################
 library(M3C)
-setwd("/Users/adityanatu/Library/Mobile Documents/com~apple~CloudDocs/Project_2023/Juliet_Data_Analysis/BV2_Juliet_EV_Proteome_Analysis/New_Norm_Data_Exosome")
+setwd("/Set/Your/Working/Dir")
 getwd()
 options(stringsAsFactors = FALSE)
 
@@ -229,42 +226,12 @@ dev.off()
 
 
 
-
-
-##################Volcanoes X 3: (LPS vs control, TGF vs control and IL10 vs control) ##################
-#this code requires Grouping variable as created + cleanDat which is your input data for abundances
-#The p value is calculated using ANOVA Tukey P value for multiple pairwise comparison - should u need the ttest equivalent p value which ignores multiple test correction - then you must subset your cleandat w/ only 2 groups
-
  
 
 #### PCA With DeX proteins: coming from toplot!
-#Sanity check and just doing a rowmatch leads to 22 unique across all comparisons!
-###Creating new cleanDat 
-#Kmeans_Expr_Matrix <- backg_nano_kmeans[backg_nano_kmeans$X %in% match_diff_ex_kmeans$V1,]
-#Write files 
-#write.csv(cleanDat, file = "ALL_cleanDat_used4PCA.csv")
-#write.csv(toplot, file = "toplot_thePCA_prots.csv")
-
-#Load files for PCA
-#cleanDat.ALL <- read.csv("ALL_cleanDat_used4PCA.csv", header = TRUE)
-#toplot.PCA <- read.csv("toplot_thePCA_prots.csv", header = TRUE)
-
-#Calculating the intersection! 
-#cleanDat.PCA_Anova <- cleanDat.ALL[cleanDat.ALL$X %in% toplot.PCA$X,]
-
-#write.csv(cleanDat.PCA_Anova, file = "PCA_prots_final.csv")
-
-#Load PCA file cleanDat 
-#cleanDat.PCA_Anova <- read.csv("PCA_prots_final.csv", header = TRUE, row.names = 1)
-
 cleanDat.PCA_Anova <- toplot
-
-
-
 #PCA of Diff Ex prots
 cleanDat.original_test <- as.data.frame(cleanDat.PCA_Anova)
-cleanDat.original_test <- as.matrix(cleanDat.PCA_Anova)
-
 cleanDat.original_test <- apply(cleanDat.original_test, 2, as.numeric)
 #Sample PCA 
 pca(cleanDat.original_test, colvec = c('skyblue', 'red', 'black', 'green'), labels = numericMeta$Treatment, controlscale=TRUE,scale=3,printres=TRUE,printwidth=25, legendtitle = "Dex_Treatment")
